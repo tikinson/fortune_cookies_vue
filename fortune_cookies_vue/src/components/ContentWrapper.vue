8a86290d44febe06f9528f6f8cbbd215cee39ea0
tikins0n <tikintikitn@gmail.com>
2024-02-18 22:07:15 +0100

    destroying template

---
+++ fortune_cookies_vue/src/components/ContentWrapper.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="item">
+
+  </div>
+
+</template>
+  
+  <style scoped>
+  
+   .item {
+    background-color: var(--main-wrapper-color);
+    min-height: 100%;
+    margin: 2rem;
+    display: flex;
+    position: relative;
+  } 
+  
+  /* @media (min-width: 1024px) {
+    .item {
+        
+    }
+  
+
+  } */
+  </style>
+  
